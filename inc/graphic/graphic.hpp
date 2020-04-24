e289507eb68393d0191e758411bb1411d437de90
Ballereau <pierre.ballereau@epitech.eu>
2020-04-24 22:43:42 +0200

    add: start dev menu

---
+++ inc/graphic/graphic.hpp
@@ -0,0 +1,72 @@
+/*
+** EPITECH PROJECT, 2020
+** GravityRun
+** File description:
+** graphic
+*/
+
+#ifndef GRAPHIC_H_
+#define GRAPHIC_H_
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Menu {
+    public:
+        Menu();
+        ~Menu();
+        sf::Sprite sprite;
+        sf::Texture texture;
+        sf::Mouse mouse;
+        std::string _username;
+        std::string _ip;
+        int _port;
+
+    private:
+    protected:
+};
+
+class Connexion{
+    public:
+        Connexion();
+        ~Connexion();
+        int connexion_page();
+        sf::Sprite sprite;
+        sf::Texture texture;
+        sf::String playerInput;
+        sf::Text playerText;
+        sf::Text text;
+        sf::Text text_username;
+        sf::Text text_ip;
+        sf::Text text_port;
+        sf::Font font;
+        std::string _ip;
+        unsigned short _port;
+    private:
+    protected:
+};
+
+class Options {
+    public:
+        Options();
+        ~Options();
+        sf::Sprite sprite;
+        sf::Texture texture;
+        sf::Text options;
+        sf::Text volume;
+        sf::Text back;
+        sf::Text volume_25;
+        sf::Text volume_50;
+        sf::Text volume_75;
+        sf::Text volume_100;
+        sf::Font font;
+    private:
+    protected:
+};
+
+#endif /* !GRAPHIC_H_ */
---
+++ src/graphic/menu.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** GravityRun
+** File description:
+** menu
+*/
+
+#include "../../inc/graphic/graphic.h"
+
+Menu::Menu()
+{
+
+    sf::IntRect rect (0, 0, 1920, 1080);
+    this->texture.loadFromFile("src/client/graphic/picture/menu.png");
+    this->sprite.setTexture(this->texture);
+    this->sprite.setTextureRect(rect);
+}
