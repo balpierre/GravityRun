8181ca9620ac2a20b2d984c617e7c630ae5a539b
brian.delalex <brian.delalex@epitech.eu>
2020-04-24 21:43:40 +0200

    Adding Player.cpp and Player.hpp

---
+++ inc/Player/Player.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** Player.hpp
+** File description:
+** 
+*/
+
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+
+class Player
+{
+public:
+    Player();
+    ~Player();
+private:
+};
+
+#endif
---
+++ src/Player/Player.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** Player.cpp
+** File description:
+** 
+*/
+
+#include "Player.hpp"
+
+Player::Player()
+{
+}
+
+Player::~Player() {}
