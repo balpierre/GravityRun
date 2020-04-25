b350ad8bc87a2299c58a720f076f60ec2a6a5c69
brian.delalex <brian.delalex@epitech.eu>
2020-04-25 09:10:21 +0200

    Adding vectors for deplacement calc.

--- inc/Player/Player.hpp
+++ inc/Player/Player.hpp
@@ -13,12 +13,14 @@
 class Player
 {
 public:
-    Player();
+    Player(sf::Vector2f pos);
     ~Player();
     sf::Vector2f get_pos();
+    void update();
 private:
     sf::Vector2f _pos;
-    sf::Vector2f _acc;
+    sf::Vector2f _speed;
+    sf::Vector2f _g;
 };
 
 #endif
